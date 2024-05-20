2e0fcd9d80153a45a5e6edfa7a67301e2ec5756d
Samarjit Swain <samarjitswain@gmail.com>
2024-05-20 15:26:07 +0000

    premitive data

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,11 @@
+"use strict" // treat all js code as newer version of javascript
+
+//alert(3+3); //it will give error as we are not running it in browser
+
+//data types 
+let name = "samarjit"//string
+let age = 18 //number
+let isloggedin = true //boolean
+let state; //undefined
+let temp = null // it is an empty value and its type is object
+console.log(typeof temp)
